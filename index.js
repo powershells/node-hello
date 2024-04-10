2b9cff9c09f11751282ebc8bb18f897a3731b91a
powershells <82601320+powershells@users.noreply.github.com>
2024-04-10 07:06:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
